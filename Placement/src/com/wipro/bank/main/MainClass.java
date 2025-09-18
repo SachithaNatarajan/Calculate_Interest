a64cdcfa4d8c60516ebadf8d30b267212222bac8
SachithaNatarajan <sachunatarajan1234@gmail.com>
2025-09-18 14:57:56 +0530

    Initial commit.

--- Placement/src/com/wipro/bank/main/MainClass.java
+++ Placement/src/com/wipro/bank/main/MainClass.java
@@ -5,7 +5,11 @@ import com.wipro.bank.service.BankService;
 public class MainClass {
      public static void main(String[] args) throws BankValidationException {
     	 Scanner sc=new Scanner(System.in);
+    	 System.out.print("Enter the number of test cases:");
+         int n=sc.nextInt();
     	 BankService service=new BankService();
+    	 for(int i=0;i<n;i++)
+    	 {
     	 System.out.print("Enter the Principal:");
     	 float principal=sc.nextFloat();
     	 System.out.print("Enter the Tenure(in years):");
@@ -14,8 +18,9 @@ public class MainClass {
     	 int age=sc.nextInt();
     	 System.out.print("Enter the Gender:");
     	 String gender=sc.next();
-    	 
-    	service.calculate(principal,tenure,age,gender);
-    	sc.close();
+    	 service.calculate(principal,tenure,age,gender);
+    	 System.out.print("\n");
+    	 }
+    	 sc.close();
     	}
 }
